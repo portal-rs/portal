fe1532555c2548025ae4e32a9068131f60611c6e
Techassi <git@techassi.dev>
2022-08-24 22:15:45 +0200

    Add ConfigError, continue Config sruct

--- src/config/errors.rs
+++ src/config/errors.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub struct ConfigError {
     kind: ConfigErrorKind,
     message: String,
@@ -10,10 +12,36 @@ pub enum ConfigErrorKind {
 }
 
 impl ConfigError {
-    pub fn new(kind: ConfigErrorKind, message: &str) -> Self {
+    pub fn new<T: Into<String>>(kind: ConfigErrorKind, message: T) -> Self {
         return ConfigError {
-            message: message.to_string(),
+            message: message.into(),
             kind,
         };
+    }
+
+    pub fn to_string(self) -> String {
+        return format!("Config error ({}): {}", self.kind, self.message);
+    }
+}
+
+impl fmt::Display for ConfigError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+impl ConfigErrorKind {
+    pub fn to_string(self) -> String {
+        match self {
+            ConfigErrorKind::Read => String::from("read"),
+            ConfigErrorKind::Write => String::from("write"),
+            ConfigErrorKind::Other => String::from("other"),
+        }
+    }
+}
+
+impl fmt::Display for ConfigErrorKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
     }
 }
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -6,11 +6,12 @@ use toml;
 
 use self::errors::{ConfigError, ConfigErrorKind};
 
-mod errors;
+pub mod errors;
 
 #[derive(Deserialize)]
 pub struct Config {
     collector: Option<CollectorOptions>,
+    resolver: Option<ResolverOptions>,
 }
 
 #[derive(Deserialize)]
@@ -22,6 +23,21 @@ pub struct CollectorOptions {
     backend: Option<String>,
 }
 
+#[derive(Deserialize)]
+pub struct ResolverOptions {
+    cache_enabled: Option<bool>,
+    upstream: Option<String>,
+    max_expire: Option<usize>,
+    hint_path: Option<String>,
+    mode: Option<String>,
+}
+
+impl Config {
+    pub fn validate(&self) -> Result<&Self, ConfigError> {
+        Ok(self)
+    }
+}
+
 pub fn default() -> Config {
     return Config {
         collector: Some(CollectorOptions {
@@ -31,14 +47,22 @@ pub fn default() -> Config {
             enabled: Some(true),
             backend: Some(String::from("default")),
         }),
+        resolver: Some(ResolverOptions {
+            cache_enabled: Some(true),
+            upstream: Some(String::from("")),
+            max_expire: Some(300),
+            hint_path: Some(String::from("")),
+            mode: Some(String::from("r")),
+        }),
     };
 }
 
-/// Read reads a TOML config file at `path` and returns a [`Config`] or any error encountered while reading
-pub fn read(path: PathBuf) -> Result<Config, ConfigError> {
+/// Read reads a TOML config file at `path` and returns a [`Config`] or any [`ConfigError`] encountered while reading
+/// and parsing. The function optionally validates the config.
+pub fn read(path: PathBuf, validate: Option<bool>) -> Result<Config, ConfigError> {
     let b = match fs::read_to_string(path) {
         Ok(b) => b,
-        Err(err) => {
+        Err(_) => {
             return Err(ConfigError::new(
                 ConfigErrorKind::Read,
                 "Failed to read config file",
@@ -48,13 +72,20 @@ pub fn read(path: PathBuf) -> Result<Config, ConfigError> {
 
     let c: Config = match toml::from_str(&b) {
         Ok(c) => c,
-        Err(err) => {
+        Err(_) => {
             return Err(ConfigError::new(
                 ConfigErrorKind::Other,
                 "Failed to parse TOML",
             ))
         }
     };
+
+    if validate.unwrap_or(false) {
+        match c.validate() {
+            Err(err) => return Err(err),
+            Ok(_) => {}
+        };
+    }
 
     Ok(c)
 }
