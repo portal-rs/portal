7f9dd0ed805da4af838c4116ee75bd5d22e073a4
Techassi <git@techassi.dev>
2022-09-13 00:59:19 +0200

    Add new modules to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::process;
 
 mod binary;
+mod client;
 mod cmd;
 mod config;
 mod constants;
+mod macros;
 mod packing;
+mod resolver;
 mod server;
 mod types;
+mod utils;
 
 fn main() {
     match cmd::execute() {
