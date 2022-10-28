2b3f0411c5073f3dcd4e54e0a7be786811d05a59
Techassi <git@techassi.dev>
2022-10-28 02:53:55 +0200

    Update DNS constants

--- src/constants/dns.rs
+++ src/constants/dns.rs
@@ -1,3 +1,3 @@
-pub const COMPRESSION_POINTER_MASK: usize = 0x3F;
-pub const MAX_DOMAIN_LENGTH: usize = 255;
-pub const MAX_LABEL_LENGTH: usize = 0x3F;
+pub const COMPRESSION_POINTER_MASK: u16 = 0x3FFF;
+pub const MAX_DOMAIN_LENGTH: u8 = 255;
+pub const MAX_LABEL_LENGTH: u8 = 0x3F;
