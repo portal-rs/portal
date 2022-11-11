8ec6ccbb962c58351bf75719bc226bd82fc4b632
Techassi <git@techassi.dev>
2022-11-11 20:04:45 +0100

    Remove unused code

--- src/types/rr/rdata/a.rs
+++
@@ -1,7 +0,0 @@
-use std::net::Ipv4Addr;
-
-use crate::packing::{UnpackBuffer, UnpackBufferResult, Unpackable};
-
-pub fn unpack(buf: &mut UnpackBuffer) -> UnpackBufferResult<Ipv4Addr> {
-    Ipv4Addr::unpack(buf)
-}
--- src/types/rr/rdata/name.rs
+++
@@ -1,8 +0,0 @@
-use crate::{
-    packing::{UnpackBuffer, UnpackBufferResult, Unpackable},
-    types::dns::Name,
-};
-
-pub fn unpack(buf: &mut UnpackBuffer) -> UnpackBufferResult<Name> {
-    Name::unpack(buf)
-}
