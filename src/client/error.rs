450f14a15c5241328f19478514f5b4b0c770ad3d
Techassi <git@techassi.dev>
2022-11-25 17:03:53 +0100

    Continue work on DNS client

--- src/client/error.rs
+++ src/client/error.rs
@@ -1,7 +1,21 @@
+use std::time;
+
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ClientError {
-    #[error("Failed to bind socket")]
-    Bind(#[from] std::io::Error),
+    #[error("IO error")]
+    IO(#[from] std::io::Error),
+
+    #[error("Failed to acquire mutex lock")]
+    WriteToBuf(#[from] crate::error::ProtocolError),
+
+    #[error("Failed to send query message: Only send partial data")]
+    SendPartial,
+
+    #[error("Bind error: Failed to create and bind UDP socket after {0:?}")]
+    BindTimeout(time::Duration),
+
+    #[error("Writing to socket timed out after {0:?}")]
+    WriteTimeout(time::Duration),
 }
--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,30 +1,30 @@
-use std::{
-    sync::{Arc, Mutex},
-    time,
+use std::time;
+
+use rand;
+use tokio::net::{ToSocketAddrs, UdpSocket};
+
+use crate::{
+    packing::{PackBuffer, Packable},
+    types::{
+        dns::{Header, Message, Name, Question},
+        rr::{Class, Type},
+    },
+    utils::{timeout, Network, TimeoutResult},
 };
-
-use tokio::net::UdpSocket;
-
-use crate::utils::network::Network;
 
 mod error;
 
 pub use error::*;
 
+pub type ClientResult<T> = Result<T, ClientError>;
+
 pub struct Client {
     network: Network,
-    dial_timeout: time::Duration,
+    bind_timeout: time::Duration,
     read_timeout: time::Duration,
     write_timeout: time::Duration,
-    header_id: Mutex<u16>,
-    socket: Arc<UdpSocket>,
-}
-
-pub struct ClientBuilder {
-    network: Network,
-    dial_timeout: time::Duration,
-    read_timeout: time::Duration,
-    write_timeout: time::Duration,
+    header_id: u16,
+    socket: UdpSocket,
 }
 
 impl Client {
@@ -32,29 +32,115 @@ impl Client {
     /// tries to bind to a random unused port for communication with other DNS
     /// servers. This is achieved by binding to port `0`, which instructs the
     /// OS to bind to an unused port.
-    pub async fn new() -> Result<Client, ClientError> {
-        let socket = match UdpSocket::bind("127.0.0.1:0").await {
-            Ok(sock) => sock,
-            Err(err) => {
-                return Err(ClientError::new(format!(
-                    "Failed to open UDP socket for DNS client: {}",
-                    err
-                )))
-            }
-        };
-        let socket = Arc::new(socket);
-
-        let client = Client {
-            network: Network::Udp,
-            dial_timeout: time::Duration::from_secs(2),
-            read_timeout: time::Duration::from_secs(2),
-            write_timeout: time::Duration::from_secs(2),
-            header_id: Mutex::new(1),
-            socket,
-        };
-
-        Ok(client)
+    pub async fn new() -> ClientResult<Client> {
+        Client::builder().build().await
     }
 
-    pub async fn dial() {}
+    /// Returns a [`ClientBuilder`] to declaratively build a [`Client`].
+    ///
+    /// ### Example
+    ///
+    /// This creates a default client and binds the socket to a random port.
+    ///
+    /// ```
+    /// use portal::client::Client;
+    ///
+    /// let c = Client::builder().build().await;
+    /// ```
+    pub fn builder() -> ClientBuilder {
+        ClientBuilder::default()
+    }
+
+    /// Sends a query to `addr` asking for `name`, `class` and `ty`.
+    ///
+    /// ### Example
+    ///
+    /// ```
+    ///
+    ///
+    ///
+    ///
+    ///
+    /// ```
+    pub async fn query(
+        &mut self,
+        name: Name,
+        class: Class,
+        ty: Type,
+        addr: impl ToSocketAddrs,
+    ) -> ClientResult<()> {
+        let id = self.get_and_refresh_header_id();
+        let header = Header::new(id);
+        let mut message = Message::new_with_header(header);
+
+        message.add_question(Question { name, class, ty });
+
+        let mut buf = PackBuffer::new();
+        if let Err(err) = message.pack(&mut buf) {
+            return Err(ClientError::WriteToBuf(err));
+        }
+
+        match timeout(self.write_timeout, self.socket.send_to(buf.bytes(), addr)).await {
+            TimeoutResult::Timeout => return Err(ClientError::WriteTimeout(self.write_timeout)),
+            TimeoutResult::Error(err) => return Err(ClientError::IO(err)),
+            TimeoutResult::Ok(_) => {}
+        }
+
+        loop {}
+    }
+
+    /// Gets the current header ID and generates a new random one.
+    fn get_and_refresh_header_id(&mut self) -> u16 {
+        let id = self.header_id;
+        self.header_id = rand::random::<u16>();
+
+        return id;
+    }
+}
+
+pub struct ClientBuilder {
+    network: Network,
+    bind_timeout: time::Duration,
+    read_timeout: time::Duration,
+    write_timeout: time::Duration,
+}
+
+impl Default for ClientBuilder {
+    fn default() -> Self {
+        Self {
+            network: Network::Udp,
+            bind_timeout: time::Duration::from_secs(2),
+            read_timeout: time::Duration::from_secs(2),
+            write_timeout: time::Duration::from_secs(2),
+        }
+    }
+}
+
+impl ClientBuilder {
+    pub async fn build(&self) -> Result<Client, ClientError> {
+        let socket = match timeout(self.bind_timeout, UdpSocket::bind("127.0.0.1:0")).await {
+            TimeoutResult::Timeout => return Err(ClientError::WriteTimeout(self.bind_timeout)),
+            TimeoutResult::Error(err) => return Err(ClientError::IO(err)),
+            TimeoutResult::Ok(socket) => socket,
+        };
+
+        Ok(Client {
+            network: self.network,
+            bind_timeout: self.bind_timeout,
+            read_timeout: self.read_timeout,
+            write_timeout: self.write_timeout,
+            header_id: 1,
+            socket,
+        })
+    }
+
+    pub fn with_bind_timeout(&mut self, bind_timeout: time::Duration) -> &Self {
+        self.bind_timeout = bind_timeout;
+        self
+    }
+
+    pub fn with_read_timeout(&mut self, read_timeout: time::Duration) -> &Self {
+        self.read_timeout = read_timeout;
+        self
+    }
 }
